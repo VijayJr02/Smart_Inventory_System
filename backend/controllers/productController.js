6c974494ab4e19cf0b24c6fc37ad08fe36f0b131
VijayJr02 <131139514+VijayJr02@users.noreply.github.com>
2025-06-17 15:40:07 +0530

    Add files via upload

---
+++ backend/controllers/productController.js
@@ -0,0 +1,9 @@
+const Product = require('../models/Product');
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+require('dotenv').config();
+const app = express();
+connectDB();
+app.use(express.json());
+app.use('/api/products', productRoutes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
